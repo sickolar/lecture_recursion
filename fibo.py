4394e75a45d6cbc910f0785932a3c71363a1d993
Andrej Kolář <andrejicek.kolar@gmail.com>
2024-04-22 12:09:56 +0200

    fiboooo

--- fibo.py
+++ fibo.py
@@ -1,5 +1,12 @@
+def recursive_nth_fibo(cislo):
+    if cislo <= 1:
+        return cislo
+    else:
+        return recursive_nth_fibo(cislo - 1) + recursive_nth_fibo(cislo - 2)
+
 
 def main():
+    print(recursive_nth_fibo(8))
     pass
 
 
