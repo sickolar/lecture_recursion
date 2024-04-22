68ae986fa4eb0fa676b2ea5effccb38e9722f31d
Andrej Kolář <andrejicek.kolar@gmail.com>
2024-04-22 13:18:51 +0200

    :((/

--- flood_fill.py
+++ flood_fill.py
@@ -1,13 +1,24 @@
 import matplotlib.pyplot as plt
 
+def flood_fill(obraz, x, y):
 
+    if x > len(obraz) - 1:
+        return obraz
+    elif y > len(obraz[0]) - 1:
+        return obraz
+    else:
+        obraz[x,y] = 2
+        obraz = flood_fill(obraz, x + 1, y)
+        obraz = flood_fill(obraz, x, y + 1)
+        obraz = flood_fill(obraz, x - 1, y)
+        obraz = flood_fill(obraz, x, y + 1)
+        return obraz
 def main():
     img = plt.imread("files/img0.png")[:, :, 0]
     # img = plt.imread("files/img1.png")[:, :, 0]
     # img = plt.imread("files/img2.png")[:, :, 0]
 
-    # img = floodfill(img, 0, 0)
-
+    img = flood_fill(img, 0, 0)
     plt.imshow(img, cmap="gray")
     plt.show(block=False)
     plt.pause(5)
